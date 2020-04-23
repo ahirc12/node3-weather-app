9d762b9366019b21a053fd996786a8ae10545e84
Ahir Chattopadhyay <ahirch1212@gmail.com>
2020-04-23 17:38:15 +0530

    added more weather details

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,24 @@
 const weatherForm = document.querySelector('form');
 const searchInput = document.querySelector('input');
 const messages = document.querySelector('#msg-1');
-const temperature = document.querySelector('#msg-2');
+const weatherDetails = document.querySelector('#msg-2');
 const weatherSummary = document.querySelector('#msg-3');
 messages.textContent = '';
-temperature.innerHTML = '';
-weatherSummary.textContent = '';
+weatherDetails.innerHTML = '';
+weatherSummary.innerHTML = '';
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const address = searchInput.value;
     const url = `/weather?address=${address}`;
-    
+
     messages.textContent = 'Loading...';
     messages.classList.remove('location');
     messages.classList.remove('error');
-    temperature.innerHTML = '';
-    weatherSummary.textContent = '';
-    
+    weatherDetails.innerHTML = '';
+    weatherSummary.innerHTML = '';
+
     fetch(url)
         .then(response => response.json())
         .then(data => {
@@ -26,12 +26,23 @@ weatherForm.addEventListener('submit', (e) => {
                 messages.textContent = data.error;
                 messages.classList.add('error');
             } else {
+                const dailyForecastData = data.forecast.daily.data;
                 messages.textContent = data.location;
                 messages.classList.add('location');
                 messages.classList.remove('error');
-                temperature.innerHTML = `<h2>${data.forecast.currently.temperature}<sup>o</sup>C</h2>`;
-                weatherSummary.textContent = data.forecast.currently.summary;
+                weatherDetails.innerHTML = `Currently it is ${generateTemperatureHTML(data.forecast.currently.temperature)}. 
+                <p>
+                    Highest temperature: ${generateTemperatureHTML(dailyForecastData[0].temperatureHigh)} 
+                </p>
+                <p>
+                    Lowest temperature: ${generateTemperatureHTML(dailyForecastData[0].temperatureLow)}
+                </p>`;
+                weatherSummary.innerHTML = `Weather Condition: <strong>${data.forecast.currently.summary}</strong>`;
             }
         })
         .catch(error => console.error(error));
 });
+
+function generateTemperatureHTML(temperature) {
+    return `<strong>${temperature}<sup>o</sup>C</strong>`;
+}
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,6 +1,6 @@
 const request = require('request');
 const geocode = (address, callback) => {
-    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoiYWhpcmMxMiIsImEiOiJjazhza2U1YmgwcW9uM2ZuczI5Mjl1Zjg2In0.vcreDhbvcsldrW9KJVOvdQ`;
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?language=en&access_token=pk.eyJ1IjoiYWhpcmMxMiIsImEiOiJjazhza2U1YmgwcW9uM2ZuczI5Mjl1Zjg2In0.vcreDhbvcsldrW9KJVOvdQ`;
     request({
         url,
         json: true
